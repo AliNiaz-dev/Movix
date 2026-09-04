99bc80481ebeb86fd66822e4ef6a03ee65dd67b2
AliNiaz-dev <aliniaz.dev@gmail.com>
2026-09-05 01:39:24 +0500

    Fix case-sensitive import for AutoScroll

--- Client/src/main.jsx
+++ Client/src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import {BrowserRouter } from 'react-router-dom'
 import './index.css'
 import App from './App.jsx'
-import AutoScroll from './components/autoScroll.jsx'
+import AutoScroll from './components/AutoScroll.jsx'
 
 
 createRoot(document.getElementById('root')).render(
